80b430cc192f5ba362f2942e59f2317b7809338f
SKAT-16 <nathangetachew536@gmail.com>
2024-10-07 21:29:12 +0300

    Add three models

--- app.py
+++ app.py
@@ -8,12 +8,14 @@ from PIL import Image
 load_dotenv()
 
 hf_token = os.getenv("HF_TOKEN")
-API_URL = "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-dev"
+API_URL_1 = "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-dev"
+API_URL_2 = "https://api-inference.huggingface.co/models/glif/how2draw"
+API_URL_3 = "https://api-inference.huggingface.co/models/CompVis/stable-diffusion-v1-4"
 headers = {"Authorization": f"Bearer {hf_token}"}
 
 
 def query(payload):
-    response = requests.post(API_URL, headers=headers, json=payload)
+    response = requests.post(API_URL_3, headers=headers, json=payload)
     return response.content
 
 
@@ -21,7 +23,7 @@ st.set_page_config(page_title="Image Generator AI", page_icon="📝", layout="ce
 st.header("Image Gen AI with black-forest-labs")
 
 input = st.text_input("Input: ", key="input")
-submit = st.button("Image Gen")
+submit = st.button("Generate")
 
 if submit:
     image_bytes = query(
